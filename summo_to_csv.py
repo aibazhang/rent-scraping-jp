0af41771fe7464c0d4f6e6a9546a033fbcb9ab22
aibazhang <dtjp@tutanota.com>
2022-01-27 21:48:53 +0900

    refactor crawler

--- summo_to_csv.py
+++ summo_to_csv.py
@@ -8,18 +8,14 @@ import pandas as pd
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-# SUUMO 検索条件
-search_url = json.load(open("./config.json", "r"))["search_url"]
-tag = json.load(open("./config.json", "r"))["tag"]
-
 def crawl_url_list(base_url):
     content = requests.get(base_url).content
     soup = BeautifulSoup(content, "html.parser")
     pages = soup.find("body").find_all("div", {"class": "pagination pagination_set-nav"})
     num_pages = int(pages[0].find_all("li")[-1].find(text=re.compile(".*?(\d+).*")))
-    return [search_url] + [search_url + "&pn=" + str(i) for i in range(2, num_pages + 1)]
+    return [base_url] + [base_url + "&pn=" + str(i) for i in range(2, num_pages + 1)]
 
-def parse_house_info(cas):
+def parse_house_info(data, cas):
     # 物件名
     subtitle = cas.find(
         "div", {"class": "cassetteitem_content-title"}
@@ -92,10 +88,7 @@ def parse_house_info(cas):
             ]
         )
 
-urls = crawl_url_list(search_url)
-data = []
-errors = []
-for url in urls:
+def parse_url(url, data, errors):
     try:
         content = requests.get(url).content
         soup = BeautifulSoup(content, "html.parser")
@@ -103,7 +96,7 @@ for url in urls:
         cassetteitems = summary.find_all("div", {"class": "cassetteitem"})
         for cas in cassetteitems:
             try:
-                parse_house_info(cas)
+                parse_house_info(data, cas)
             except Exception as e:
                 errors.append([e, url, len(data)])
                 pass
@@ -114,51 +107,69 @@ for url in urls:
         errors.append([e, url, len(data)])
         pass
 
-house_info_df = pd.DataFrame(
-    data,
-    columns=[
-        "物件名",
-        "住所",
-        "立地1",
-        "立地2",
-        "立地3",
-        "築年数",
-        "階数",
-        "物件階",
-        "家賃",
-        "管理費",
-        "敷金",
-        "礼金",
-        "間取り",
-        "面積",
-    ],
-)
-
-
-datetime_now = datetime.now()
-
-date_str = datetime_now.strftime("%Y_%m_%d_%H_%M")
-house_info_df["datetime"] = datetime_now
-house_info_df = (
-    house_info_df.drop_duplicates(subset=["立地1", "家賃", "住所", "築年数"], keep="first")
-    .drop_duplicates(subset=["物件名"], keep="last")
-    .reset_index(drop=True)
-)
-
-house_info_df.to_csv(
-    "{}/data/{}_{}.csv".format(
-        os.path.dirname(__file__),
-        date_str,
-        tag,
+def post_process(data, errors, tag):
+    house_info_df = pd.DataFrame(
+        data,
+        columns=[
+            "物件名",
+            "住所",
+            "立地1",
+            "立地2",
+            "立地3",
+            "築年数",
+            "階数",
+            "物件階",
+            "家賃",
+            "管理費",
+            "敷金",
+            "礼金",
+            "間取り",
+            "面積",
+        ],
     )
-)
 
-if len(errors) > 0:
-    errors_df = pd.DataFrame(errors)
-    errors_df.to_csv(
-        "{}/error/{}_{}.csv".format(
+
+    datetime_now = datetime.now()
+
+    date_str = datetime_now.strftime("%Y_%m_%d_%H_%M")
+    house_info_df["datetime"] = datetime_now
+    house_info_df = (
+        house_info_df.drop_duplicates(subset=["立地1", "家賃", "住所", "築年数"], keep="first")
+        .drop_duplicates(subset=["物件名"], keep="last")
+        .reset_index(drop=True)
+    )
+
+    house_info_df.to_csv(
+        "{}/data/{}_{}.csv".format(
             os.path.dirname(__file__),
             date_str,
             tag,
         )
     )
+
+    if len(errors) > 0:
+        errors_df = pd.DataFrame(errors)
+        errors_df.to_csv(
+            "{}/error/{}_{}.csv".format(
+                os.path.dirname(__file__),
+                date_str,
+                tag,
+            )
+        )
+
+
+def main():
+    # SUUMO 検索条件
+    search_url = json.load(open("./config.json", "r"))["search_url"]
+    tag = json.load(open("./config.json", "r"))["tag"]
+
+    urls = crawl_url_list(search_url)
+    data = []
+    errors = []
+    for url in urls:
+        parse_url(url, data, errors)
+    post_process(data, errors, tag)
+
+
+if __name__ == "__main__":
+    main()
