53beba9a1afed9c30a6ccab29a1cfebb52e467b9
aibazhang <dtjp@tutanota.com>
2022-01-29 16:17:52 +0900

    add histories

--- analyze.py
+++ analyze.py
@@ -90,11 +90,19 @@ def analyze_rent():
         newest_rentable_frame[columns].to_csv("./results/{}/rentable_houses.csv".format(tag))
 
         newest_frame = get_newest_frame(data, only_avaliable=False)
-        for name in newest_frame[newest_frame["rental_cost_changed"]].index.tolist():
+
+
+        cost_change_list = newest_frame[newest_frame["rental_cost_changed"]].index.tolist()
+        cost_not_change_list = newest_frame[~newest_frame["rental_cost_changed"]].index.tolist()
+        
+        for name in cost_change_list:
+            data[data["物件名"] == name].sort_values(by="date")[price_columns].to_csv(
+                "./results/{}/CHANGED_{}.csv".format(tag, name)
+            )
+        for name in cost_not_change_list:
             data[data["物件名"] == name].sort_values(by="date")[price_columns].to_csv(
                 "./results/{}/{}.csv".format(tag, name)
             )
-
 
 if __name__ == "__main__":
     analyze_rent()
